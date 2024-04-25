4f7a5ad0f279159a319f4e6e02c935eae81d6218
vlomeli <victorlomeli2023@gmail.com>
2024-04-25 16:20:00 -0700

    Updated Delete / Create Comment
    I added incrementing when creating a comment as well as decrementing when the comment is deleted. I didnt want to change much of the persons code so I really just changed what was in db folder. Also there were two routes for deleting comments and I got rid of one.

--- src/router/posts.js
+++ src/router/posts.js
@@ -47,7 +47,6 @@ export default (router) => {
     isCommentOwner,
     deleteComment
   );
-  router.delete('/posts/:id/comment', isAuthenticated, deleteComment);
   router.delete(
     '/posts/:id/delete',
     isAuthenticated,
