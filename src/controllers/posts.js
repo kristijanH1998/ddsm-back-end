1eef11482e82a63ed63a0cf64ae73aa52e561894
kristijanH1998 <kristijan.hornung1@gmail.com>
2024-04-25 03:17:29 -0700

    all functionality implemented

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -157,7 +157,20 @@ export const getPostLikeCount = async (req, res) => {
 }
 
 export const getLikesForPost = async(req, res) => {
-
+  try {
+    const post_id = req.params.id;
+    const lim = Number(req.params.lim);
+    const step = Number(req.params.step);
+    if(lim <= 0 || step < 0) {
+      return res.status(400).json({ error: 'Limit must be greater than 0 and step greater than or equal to 0.' });
+    }
+    const likes = await getPostLikes(post_id, lim, step);
+    const userIds = await getUsernamesAndPics({type:'likes', content: likes});
+    return res.status(200).json(userIds);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 }
 export const getCommsForPost = async (req, res) => {
   try {
@@ -168,7 +181,7 @@ export const getCommsForPost = async (req, res) => {
       return res.status(400).json({ error: 'Limit must be greater than 0 and step greater than or equal to 0.' });
     }
     const comments = await getCommentsForPost(post_id, lim, step);
-    const userIds = await getUsernamesAndPics(comments);
+    const userIds = await getUsernamesAndPics({type:'comments', content: comments});
     return res.status(200).json(userIds);
   } catch (error) {
     console.error(error);
